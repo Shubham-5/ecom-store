3eab9a651abc3a0ee44b1efbc43cc5c83aafca68
Shubham Girase <sgirase08@gmail.com>
2024-12-10 11:21:45 +0530

    Add debounce util function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+
+export const debounce = (func, delay) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
